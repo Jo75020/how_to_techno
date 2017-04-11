c0feb3142b6874957f7845af6cf45a949a227d20
Jordan Demmouche <jordand75020@hotmail.fr>
2017-04-11 17:00:45 +0200

    init commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,17 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @user = User.new
+
+  end
+
+private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name)
   end
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'jordan@lestudio.com'
   layout 'mailer'
 end
