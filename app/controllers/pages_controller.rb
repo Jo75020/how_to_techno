0d34fcfb5e8d277d52291f0bb5fc8619484b7a0e
Jordan Demmouche <jordand75020@hotmail.fr>
2017-04-12 20:02:46 +0200

    add program

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,11 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!
 
   def home
     @info  = Info.new
+  end
+
+  def programme
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :infos
   get "infos",     to: "pages#home"
   post "infos",     to: "pages#home"
+  get "programme", to: "pages#programme"
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
